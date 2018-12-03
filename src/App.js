c92d22d87b956e6ae88de34eb2bf3b752d8f8ef7
Manuel Venegas <mengasis@outlook.com>
2018-12-03 13:40:15 -0300

    [WIP] App component renders SWAPI response

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
+
+import List from './components/List'
+import Box from './components/Box'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const Title = styled.span`
+  font-size: 45px;
+  font-weight: 100;
+`
 
 class App extends Component {
+  state = { films: [] }
+
+  async componentDidMount() {
+    const { data } = await axios.get('https://swapi.co/api/films/?format=json')
+    const films = data.results.map(film => ({ id: film.episode_id, title: film.title })) || []
+
+    this.setState({ films })
+  }
+
   render() {
+    const { films } = this.state
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Container>
+        <Title>Films Star Wars</Title>
+        <List>
+          {films.map(film => (
+            <Box key={film.id} {...film} />
+          ))}
+        </List>
+      </Container>
+    )
   }
 }
 
-export default App;
+export default App
