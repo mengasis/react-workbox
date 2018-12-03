0fe7f0bad284ca28792bb85c08eb59cf3e735ec0
Manuel Venegas <mengasis@outlook.com>
2018-12-03 13:40:28 -0300

    Created Box and List components

---
+++ src/components/Box.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const Container = styled.li``
+
+const Link = styled.a``
+
+const Box = ({ id, title }) => (
+  <Container>
+    <Link href={`https://swapi.co/api/films/${id}/?format=json`}>{title}</Link>
+  </Container>
+)
+
+Box.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+}
+
+export default Box
