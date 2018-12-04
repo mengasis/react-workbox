efde171ef50cb4ae2a6d09c9c3898701031c64c5
Manuel Venegas <mengasis@outlook.com>
2018-12-03 21:50:27 -0300

    [Fix] Align items

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export default styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  align-items: center;
   height: 100vh;
 `
--- src/components/List.js
+++ src/components/List.js
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 
 export default styled.ul`
   display: grid;
-  grid-template: 1fr / repeat(2, 1fr);
   gap: 10px;
   list-style: none;
+  justify-items: center;
+  padding: 0;
 `
